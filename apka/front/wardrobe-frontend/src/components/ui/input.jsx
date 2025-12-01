341cbdfa33c93e32ffbc894e111170a6625dd50d
JanGniedziejko <169074257+JanGniedziejko@users.noreply.github.com>
2025-12-01 08:20:05 +0100

    delete old react app

--- apka/front/wardrobe-frontend/src/components/ui/input.jsx
+++
@@ -1,9 +0,0 @@
-export function Input(props) {
-    return (
-      <input
-        className="w-full border border-gray-300 rounded px-2 py-1"
-        {...props}
-      />
-    );
-  }
-  
--- apka/front/wardrobe-frontend/src/components/ui/label.jsx
+++
@@ -1,4 +0,0 @@
-export function Label({ children }) {
-    return <label className="block text-sm font-medium mb-1">{children}</label>;
-  }
-  
