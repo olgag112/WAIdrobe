ba28182b4991c63d3b6948652b5eefa1edf4c372
olgag112 <olga@internetum.pl>
2025-12-11 18:41:54 +0100

    added comments

--- apka/backend/network/train/model.py
+++ apka/backend/network/train/model.py
@@ -1,22 +1,29 @@
 import torch
 import torch.nn as nn
 
+    # Neural network for outfit score prediction.
+    # Combines:
+    #   - Categorical features encoded via embeddings
+    #   - Numerical weather + favorite features
+    
 class RecommenderNet(nn.Module):
     def __init__(self, cat_dims, emb_dims, num_input_dim):
         super().__init__()
 
-        # Embedding warstwy, dla kazdej kategorii opsowej tworzy sie maciez ktorej elementy modyfikuja sie podczas uczenia -- na koniec lepsza postac maciezy
+        # Embedding layers — one embedding per categorical feature
+        # Embeddings are trainable and learned during training
         self.embeddings = nn.ModuleList([
             nn.Embedding(cat_dim, emb_dim) for cat_dim, emb_dim in zip(cat_dims, emb_dims)
         ])
 
-        # Wyjściowy wymiar wektora cech - liczba embedingow + liczba kategorii numerycznych
+         # Total input size to the MLP:
+        #   sum(embedding dims) + numerical feature count
         input_dim = sum(emb_dims) + num_input_dim
-        # Prosty MLP
+        
         self.model = nn.Sequential(
             nn.Linear(input_dim, 128),
-            nn.ReLU(),  # funckcja aktywacji, wprowadza nieliniowosc
-            nn.Linear(128, 64),  # new hidden layer
+            nn.ReLU(),  
+            nn.Linear(128, 64),  
             nn.ReLU(),
             nn.Linear(64, 32),
             nn.ReLU(),
@@ -24,8 +31,7 @@ class RecommenderNet(nn.Module):
         )
 
     def forward(self, cat_data, num_data):
-        # tensory embedingowe
         emb = [emb_layer(cat_data[:, i]) for i, emb_layer in enumerate(self.embeddings)]
-        # jeden tensor wejsciowy ze wszytskimi embedingami i danymi numerycznymi
+        # Concatenate all embeddings, numeric features
         x = torch.cat(emb + [num_data], dim=1)
         return self.model(x).squeeze(1)
